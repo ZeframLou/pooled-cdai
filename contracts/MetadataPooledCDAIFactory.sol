19aba58c058e8c4318c82389745e4cc0d3bcc41c
Zefram Lou <zeframlou@gmail.com>
2019-08-05 17:10:08 +0800

    added factory for creating pcDAI with metadata (such as IPFS hash)

---
+++ contracts/MetadataPooledCDAIFactory.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "./PooledCDAIFactory.sol";
+
+contract MetadataPooledCDAIFactory is PooledCDAIFactory {
+  event CreatePoolWithMetadata(address sender, address pool, bool indexed renounceOwnership, bytes metadata);
+
+  function createPCDAIWithMetadata(
+    string memory name,
+    string memory symbol,
+    address _beneficiary,
+    bool renounceOwnership,
+    bytes memory metadata
+  ) public returns (PooledCDAI) {
+    PooledCDAI pcDAI = _createPCDAI(name, symbol, _beneficiary, renounceOwnership);
+    emit CreatePoolWithMetadata(msg.sender, address(pcDAI), renounceOwnership, metadata);
+  }
+}
--- contracts/PooledCDAIFactory.sol
+++ contracts/PooledCDAIFactory.sol
@@ -6,13 +6,18 @@ contract PooledCDAIFactory {
   event CreatePool(address sender, address pool, bool indexed renounceOwnership);
 
   function createPCDAI(string memory name, string memory symbol, address _beneficiary, bool renounceOwnership) public returns (PooledCDAI) {
+    PooledCDAI pcDAI = _createPCDAI(name, symbol, _beneficiary, renounceOwnership);
+    emit CreatePool(msg.sender, address(pcDAI), renounceOwnership);
+    return pcDAI;
+  }
+
+  function _createPCDAI(string memory name, string memory symbol, address _beneficiary, bool renounceOwnership) internal returns (PooledCDAI) {
     PooledCDAI pcDAI = new PooledCDAI(name, symbol, _beneficiary);
     if (renounceOwnership) {
       pcDAI.renounceOwnership();
     } else {
       pcDAI.transferOwnership(msg.sender);
     }
-    emit CreatePool(msg.sender, address(pcDAI), renounceOwnership);
     return pcDAI;
   }
 }
