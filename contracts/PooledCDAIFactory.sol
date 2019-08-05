4403309cca06dd0bd82bf3bafd1a005aa3c12154
Zefram Lou <zeframlou@gmail.com>
2019-08-05 13:28:34 +0800

    added renounceOwnership option in factory

--- contracts/PooledCDAIFactory.sol
+++ contracts/PooledCDAIFactory.sol
@@ -3,12 +3,16 @@ pragma solidity >=0.4.21 <0.6.0;
 import "./PooledCDAI.sol";
 
 contract PooledCDAIFactory {
-  event CreatePool(address sender, address pool);
+  event CreatePool(address sender, address pool, bool indexed renounceOwnership);
 
-  function createPCDAI(string memory name, string memory symbol, address _beneficiary) public returns (PooledCDAI) {
+  function createPCDAI(string memory name, string memory symbol, address _beneficiary, bool renounceOwnership) public returns (PooledCDAI) {
     PooledCDAI pcDAI = new PooledCDAI(name, symbol, _beneficiary);
-    pcDAI.transferOwnership(msg.sender);
-    emit CreatePool(msg.sender, address(pcDAI));
+    if (renounceOwnership) {
+      pcDAI.renounceOwnership();
+    } else {
+      pcDAI.transferOwnership(msg.sender);
+    }
+    emit CreatePool(msg.sender, address(pcDAI), renounceOwnership);
     return pcDAI;
   }
 }
